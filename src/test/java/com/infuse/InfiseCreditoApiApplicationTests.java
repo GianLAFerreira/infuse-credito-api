2dcd19b5e05751992559fd7c4b33a4290e2c3104
Gian Lucas Alves Ferreira <78167245+GianLAFerreira@users.noreply.github.com>
2025-05-12 20:54:39 -0300

    Commit inicial do projeto

---
+++ src/test/java/com/infuse/InfiseCreditoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infuse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfiseCreditoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
